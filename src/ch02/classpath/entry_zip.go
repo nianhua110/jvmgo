2959815333438ef9d0596e088b124fada94b2411
Huang Kai <hk@ciphergateway.com>
2017-10-12 10:51:02 +0800

    Initial commit

---
+++ src/ch02/classpath/entry_zip.go
@@ -0,0 +1,45 @@
+package classpath
+
+import "io/ioutil"
+import "archive/zip"
+import "errors"
+import "path/filepath"
+
+type ZipEntry struct {
+	absDir string
+}
+
+func newZipEntry(path string) *ZipEntry {
+	absDir, err := filepath.Abs(path)
+	if err != nil {
+		panic(err)
+	}
+	return &ZipEntry{absDir}
+}
+
+func (self *ZipEntry) readClass(className string) ([]byte, Entry, error) {
+	r, err := zip.OpenReader(self.absDir)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer r.Close()
+	for _, f := range r.File {
+		if f.Name == className {
+			rc, err := f.Open()
+			if err != nil {
+				return nil, nil, err
+			}
+			defer rc.Close()
+			data, err := ioutil.ReadAll(rc)
+			if err != nil {
+				return nil, nil, err
+			}
+			return data, self, nil
+		}
+	}
+	return nil, nil, errors.New("classs not found: " + className)
+}
+
+func (self *ZipEntry) String() string {
+	return self.absDir
+}
